cee3f16207e2ccbb0314e7c18bb58e537b2d3f4a
NTTVy03 <trucvy05062003@gmail.com>
2023-11-25 21:44:40 +0700

    Login - basic layout

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -12,53 +12,89 @@ class _LoginPage extends State<LoginPage> {
   static const routeName = '/profile-screen';
 
   final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
-  void handleTextFieldOnTap(String text) {
+  void handleForgotPassword() {
     print(
         "#############################################################################");
-    print(text);
+    print("You forgot your password???");
+  }
+
+  void handleLogin() {
+    print(
+        "#############################################################################");
+    print(_emailController.text);
+    print(_passwordController.text);
     print(
         "#############################################################################");
   }
 
   @override
   Widget build(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width;
-    var screenHeight = MediaQuery.of(context).size.height;
+    // var screenWidth = MediaQuery.of(context).size.width;
+    // var screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
         body: SafeArea(
-            child: Column(
-      children: [
-        const Center(
-            child: Text(
-          "Login",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        )),
-        InkWell(
-          onTap: () {
-            print(
-                "Wtf why is it not working????????????????????????????????????????");
-          },
-          child: Text("Hello?"),
+            child: Column(children: [
+      const Center(
+          child: Text(
+        "Login",
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+      )),
+      const Text(
+        "Login to your account to continue",
+        style: TextStyle(color: Color.fromARGB(100, 1, 1, 1)),
+      ),
+      MaterialTextField(
+        keyboardType: TextInputType.emailAddress,
+        hint: 'Email',
+        labelText: 'Email',
+        textInputAction: TextInputAction.next,
+        prefixIcon: const Icon(Icons.email_outlined),
+        controller: _emailController,
+        // onChanged: (text) => handleTextFieldOnTap(text),
+      ),
+      MaterialTextField(
+        keyboardType: TextInputType.visiblePassword,
+        hint: 'Password',
+        labelText: 'Password',
+        textInputAction: TextInputAction.done,
+        obscureText: true,
+        prefixIcon: const Icon(Icons.lock),
+        // suffixIcon: const Icon(Icons.visibility),
+        controller: _passwordController,
+      ),
+      Align(
+        alignment: Alignment.centerRight,
+        child: Container(
+            margin: const EdgeInsets.all(16),
+            child: InkWell(
+                onTap: () => handleForgotPassword(),
+                child: const Text(
+                  'Forgot password?',
+                  style: TextStyle(
+                      fontSize: 14, color: Color.fromARGB(255, 33, 70, 219)),
+                ))),
+      ),
+      Container(
+        width: 120,
+        height: 40,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 33, 70, 219),
+          borderRadius: BorderRadius.circular(20),
         ),
-        InkWell(
-          onTap: () {
-            debugPrint("Hello from here?????");
-          },
-          child: Center(
-            child: MaterialTextField(
-              keyboardType: TextInputType.emailAddress,
-              hint: 'Email',
-              labelText: 'Email',
-              textInputAction: TextInputAction.next,
-              prefixIcon: Icon(Icons.email_outlined),
-              controller: _emailController,
-              onChanged: (text) => handleTextFieldOnTap(text),
+        child: TextButton(
+          onPressed: () => handleLogin(),
+          child: const Text(
+            'Login',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
             ),
           ),
         ),
-      ],
-    )));
+      )
+    ])));
   }
 }
